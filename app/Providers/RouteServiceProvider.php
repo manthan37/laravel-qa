7dcc25728399cc0b4f49ce8a7249fdb2af94ce5e
manthan37 <manthankyada37@gmail.com>
2020-12-17 10:30:20 +0530

    Implemented show functionality

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Question;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -35,6 +36,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Route::bind('slug', function ($slug) {
+            return  Question::where('slug', $slug)->first() ?? abort(404);
+        });
         $this->configureRateLimiting();
 
         $this->routes(function () {
