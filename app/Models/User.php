452107cb43f1d05da34dbdf801d31b1dbb2f8f2b
manthan37 <manthankyada37@gmail.com>
2020-12-18 17:09:19 +0530

    Design Answer Schema

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,8 @@ class User extends Authenticatable
         // return route('questions.show', $this->id);
         return '#';
     }
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
