704891a4c5e191c113093efe0f124a8d59aaf3c3
manthan37 <manthankyada37@gmail.com>
2020-12-19 12:19:00 +0530

    implement update answer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Answer;
 use App\Models\Question;
 use App\Models\User;
+use App\Policies\AnswerPolicy;
 use App\Policies\QuestionPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -17,6 +19,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Question::class => QuestionPolicy::class,
+        Answer::class => AnswerPolicy::class,
     ];
 
     /**
--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -23,18 +23,36 @@
                     </div>
                     <div class="media-body">
                         {!! $answer->body_html !!}
-                        <div class="float-right">
-                            <span class="text-muted">
-                                Answerd By {{ $answer->created_date }}
-                            </span>
-                            <div class="media mt-2">
-                                <a href="{{ $answer->user->url }}" class="pr-2"><img
-                                        src="{{ $answer->user->avatar }}" height="32" alt=""></a>
-                                <div class="media-body mt-1">
-                                    <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
+                        <div class="row">
+                            <div class="col-4">
+                                <div class="ml-auto">
+                                    @can('update',$answer)
+                                    <a href="{{ route('questions.answers.edit',[$answer->id,$question->id]) }}" class="btn btn-sm  btn-outline-info">Edit</a>
+                                    @endcan
+                                    @can('delete',$answer)
+                                    <form action="{{ route('questions.answers.destroy',[$answer->id,$question->id]) }}" method="post" class="form-delete">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button type="submit" value="Delete" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are You Sure?')">Delete</button>
+                                    </form>
+                                    @endcan
+                                </div>
+                            </div>
+                            <div class="col-4"></div>
+                            <div class="col-4">
+                                <span class="text-muted">
+                                    Answerd By {{ $answer->created_date }}
+                                </span>
+                                <div class="media mt-2">
+                                    <a href="{{ $answer->user->url }}" class="pr-2"><img
+                                            src="{{ $answer->user->avatar }}" height="32" alt=""></a>
+                                    <div class="media-body mt-1">
+                                        <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
                 <hr>
