d3c2c6496d4db5966b9ccb9f529f57f96fbfafc3
manthan37 <manthankyada37@gmail.com>
2020-12-21 12:33:36 +0530

    Securing the application ftom XSS attack

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -40,7 +40,7 @@ class Question extends Model
     }
     public function getBodyHtmlAttribute()
     {
-        return \Parsedown::instance()->text($this->body);
+        return clean($this->bodyHtml());
     }
     public function answers()
     {
@@ -66,5 +66,17 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
+    }
+    public function getExcerptAttribute()
+    {
+        return $this->excerpt(250);
+    }
+    public function excerpt($length)
+    {
+        return Str::limit(strip_tags($this->bodyHtml()), $length, ' ...');
+    }
+    private function bodyHtml()
+    {
+        return \Parsedown::instance()->text($this->body);
     }
 }
